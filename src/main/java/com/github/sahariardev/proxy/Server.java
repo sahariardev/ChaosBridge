3479dbfda9eaeb74800e24fa3de69c084297e0bd
sahariardev <rifatsahariar@gmail.com>
2025-04-04 11:57:34 +0600

    Cleanup all rnd

--- src/main/java/com/github/sahariardev/proxy/Server.java
+++ src/main/java/com/github/sahariardev/proxy/Server.java
@@ -1,46 +1,8 @@
 package com.github.sahariardev.proxy;
 
-import com.github.sahariardev.proxy.ReactorNettyChunkedTcpProxy.BandwidthLimitHandler;
-import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.EventLoopGroup;
-import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.SocketChannel;
-import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.logging.LogLevel;
-import io.netty.handler.logging.LoggingHandler;
-
-import static com.github.sahariardev.proxy.ProxyServerHandler.eventLoopGroup;
-
 public class Server {
 
     public void start(int port, String serverHost, int serverPort) {
-
-        EventLoopGroup bossGroup = new NioEventLoopGroup();
-        EventLoopGroup workerGroup = new NioEventLoopGroup();
-
-        try {
-            ServerBootstrap bootstrap = new ServerBootstrap();
-            bootstrap.group(bossGroup, workerGroup)
-                    .channel(NioServerSocketChannel.class)
-                    .handler(new LoggingHandler(LogLevel.INFO))
-                    .childHandler(new ChannelInitializer<SocketChannel>() {
-                        @Override
-                        protected void initChannel(SocketChannel channel) {
-                            channel.pipeline().addLast(new ProxyServerHandler(serverHost, serverPort))
-                                    .addLast(new BandwidthLimitHandler());
-                        }
-                    });
-
-            ChannelFuture future = bootstrap.bind(port).sync();
-            future.channel().closeFuture().sync();
-        } catch (Exception e) {
-            e.printStackTrace();
-            bossGroup.shutdownGracefully();
-            workerGroup.shutdownGracefully();
-            eventLoopGroup.shutdownGracefully();
-        }
 
     }
 }

eafe0dc346e9daa75d4a52d61e508d00e6ddba5a
sahariardev <rifatsahariar@gmail.com>
2025-04-04 13:40:14 +0600

    Added sl4j logging

--- src/main/java/com/github/sahariardev/Application.java
+++ src/main/java/com/github/sahariardev/Application.java
@@ -1,11 +1,16 @@
 package com.github.sahariardev;
 
 import com.github.sahariardev.proxy.Server;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
-public class Main {
+public class Application {
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) throws IOException {
+        logger.info("Application started");
         Server server = new Server();
         server.start(1081, "httpforever.com", 80);
     }

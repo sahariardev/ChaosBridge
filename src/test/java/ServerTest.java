854f94bca9be3c7693e90d9582f06394c9ecb90b
sahariardev <rifatsahariar@gmail.com>
2025-05-13 15:50:07 +0600

    Refactored test connectivity

--- src/test/java/ServerTest.java
+++ src/test/java/ServerTest.java
@@ -1,4 +1,6 @@
 import com.github.sahariardev.proxy.Server;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -13,17 +15,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ServerTest {
 
     private final int port = 7080;
-
     private final String serverHost = "localhost";
-
     private final int serverPort = 7081;
-
     private final String key = "7080:localhost:7081";
 
-    @Test
-    void testConnectivity() throws IOException, InterruptedException {
-        String message = "ping";
-        ServerSocket serverSocket = new ServerSocket(serverPort);
+    private ServerSocket serverSocket;
+    private Server server;
+    private Socket clientSocket;
+
+    private final String message = "hello";
+
+    @BeforeEach
+    void setup() throws IOException, InterruptedException {
+        serverSocket = new ServerSocket(serverPort);
 
         Thread serverSocketThread = new Thread(() -> {
             try (Socket clientSocket = serverSocket.accept();
@@ -42,7 +46,7 @@ public class ServerTest {
 
         serverSocketThread.start();
 
-        Server server = new Server(port, serverHost, serverPort, key);
+        server = new Server(port, serverHost, serverPort, key);
 
         Thread serverThread = new Thread(() -> {
             try {
@@ -55,8 +59,18 @@ public class ServerTest {
 
         Thread.sleep(500);
 
-        Socket clientSocket = new Socket("localhost", port);
+        clientSocket = new Socket("localhost", port);
+    }
 
+    @AfterEach
+    void tearDown() throws IOException {
+        serverSocket.close();
+        server.stop();
+        clientSocket.close();
+    }
+
+    @Test
+    void testConnectivity() throws IOException, InterruptedException {
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
         bufferedWriter.write("Hello\n");
         bufferedWriter.flush();
@@ -68,7 +82,5 @@ public class ServerTest {
         bufferedWriter.close();
         bufferedReader.close();
         clientSocket.close();
-        server.stop();
-        serverSocket.close();
     }
 }
